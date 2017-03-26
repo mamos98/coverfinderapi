bd5a47e660bf56a998f098e87b35e459cf04da49
Masoud Mirzaei <mamos98@gmail.com>
2017-03-27 02:31:09 +0430

    Add Wrappers with Promise and CORS module

--- index.js
+++ index.js
@@ -1,20 +1,76 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    rp      = require('request-promise'),
+    cors    = require('cors'),
+    app     = express();
 
+// Set Main Configurations
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
+// Set CORS
+var whitelist = ['http://localhost:4200/', 'http://masoudmirzei.ir/'];
+var corsOptions = {
+  origin: function(origin, callback){
+    var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+    callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+  }
+};
+app.use(cors());
+
+// Set View Configurations
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
-app.get('/', function(request, response) {
+
+// Set Routes
+// Show Landing Page
+app.get('/', function (request, response) {
   response.render('main/landing');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/api', function (request, response) {
+  response.json({
+    message: 'This is a Wrapper on Spotify Open APIs for http://masoudmirzei.ir/coverfinder/'
+  });
+});
+
+// Set API
+app.get('/api/search', function (request, response) {
+  return rp({
+    uri: 'https://api.spotify.com/v1/search?q='+ request.query.q + '&type=' + request.query["type"] || 'album',
+    json: true
+  }).then(
+    function successCallback(result) {
+      return response.json(result);
+    },
+    function errorCallback() {
+      response.status(500);
+      response.json({
+        message: 'There Was An Error while getting Information from Spotify'
+      });
+    }
+  )
+});
+
+app.get('/api/albums/:album_id', function (request, response) {
+  return rp({
+    uri: 'https://api.spotify.com/v1/albums/' + request.params.album_id,
+    json: true
+  }).then(
+    function successCallback(result) {
+      return response.json(result);
+    },
+    function errorCallback() {
+      response.status(500);
+      response.json({
+        message: 'There Was An Error while getting Information from Spotify'
+      });
+    }
+  )
 });
 
 
+// Run Application at Last
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
